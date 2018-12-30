1b7e647d0cd703284266c3c6e3a2a93c7ea3c7d9
jaywelborn <jesse.welborn@gmail.com>
2018-12-30 16:22:29 -0500

    Replaced unnecessary permission code in JobApplication app

--- jobtracker/jobapplication/permissions.py
+++ jobtracker/jobapplication/permissions.py
@@ -9,8 +9,23 @@ class IsOwnerOrAdmin(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser or request.user == obj.creator:
-            return True
-        raise PermissionDenied({
-            "message": "Access Forbidden"
-        })
+        """
+        Determine if the user making request has permission to see the requested
+        object. Currently, the application filters all objects not created by
+        the requester out of the queryset before they can be retrieved.
+
+        This renders this permission somewhat unnecessary. If non-creators are
+        allowed to view objects in the future, replace the method body with the
+        following:
+            if request.user.is_superuser or request.user == obj.creator:
+                return True
+            raise PermissionDenied({
+                "message": "Access Forbidden"
+            })
+
+        :param request: HTTP request
+        :param view: View responsible for returning response
+        :param obj: Object to check permission
+        :return: True if user is superuser, or the object's creator
+        """
+        return request.user.is_superuser or request.user == obj.creator

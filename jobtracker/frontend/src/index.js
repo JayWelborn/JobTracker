7f34b566951bf709e9d3e619d4fceb156a1fb1ad
jaywelborn <jesse.welborn@gmail.com>
2019-03-06 21:30:56 -0500

    Set up webpack with browser-sync and sass loader

--- jobtracker/frontend/src/index.js
+++ jobtracker/frontend/src/index.js
@@ -1,1 +1,2 @@
+import "./styles/main.css"
 import App from "./components/App";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   module: {
     rules: [
@@ -7,7 +10,33 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
-  }
+  },
+
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:8000/',
+      files: [
+        "**/main.css",
+        "**/*.js",
+        "**/*.html",
+      ]
+    })
+  ]
 };
